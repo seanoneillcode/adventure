f58150aa6bca719c9d8c0f3a52b3ff896b52375c
Sean O'Neill <seansaysmeh@gmail.com>
2018-09-24 21:52:19 +0100

    moved everything about and made it 3d ish

---
+++ Assets/Scripts/cahrcontroller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cahrcontroller : MonoBehaviour {
+
+    public SpriteRenderer spriteRenderer;
+    public Animator animator;
+    public float speed;
+    public float turningSpeed = 60;
+    public float movementSpeed = 10;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        //var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        //if (Mathf.Abs(x) > 0)
+        //{
+        //    spriteRenderer.flipX = x < 0;
+        //}
+
+        //transform.Translate(x, 0, 0);
+        //transform.Translate(0, 0, z);
+
+
+        float horizontal = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        float h = turningSpeed * Input.GetAxis("Mouse X");
+        transform.Rotate(0, h, 0);
+
+        float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        transform.Translate(horizontal, 0, vertical);
+
+        animator.SetBool("isWalking", Mathf.Abs(vertical) + Mathf.Abs(horizontal) > 0);
+    }
+}
---
+++ Assets/Scripts/cameraContraolet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraContraolet : MonoBehaviour {
+
+    public GameObject target;
+    public float damping = 0.01f;
+    Vector3 offset;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        offset = target.transform.position - transform.position;
+    }
+
+    void LateUpdate()
+    {
+
+        float desiredAngle = target.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+
+        Vector3 desiredPosition = target.transform.position - (rotation * offset);
+        Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
+        transform.position = position;
+        //transform.position = target.transform.position - (rotation * offset);
+
+        transform.LookAt(target.transform);
+    }
+}
